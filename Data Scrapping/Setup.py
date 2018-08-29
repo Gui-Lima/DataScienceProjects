68bc293d0fe9818ac6351fb841400debecf43419
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-29 19:50:02 -0300

    Returning list from the findings model and capacity

--- Data Scrapping/Setup.py
+++ Data Scrapping/Setup.py
@@ -8,7 +8,29 @@ import re
 url = "/home/ggfl/Github/DataScienceProjects/Data Scrapping/DataSets/galaxyS8PageZoom"
 parsed_page = BeautifulSoup(open(url),"html.parser")
 
-for a in parsed_page.findAll('a', {'class':"name-link"}):
-    regexSamsung = "(Galaxy S\d(?:\sPlus)?)\s*SM-G(?:\d+[a-zA-Z]*)\s*(?:\dGB\s*RAM)*\s*(\d+)GB"
-    teste = re.sub('Smartphone Samsung ', '', a.text)
-    modelo, capacidade = re.match(regexSamsung, teste).groups()
+
+regexSamsung = "(Galaxy S\d(?:\sPlus)?)\s*SM-G(?:\d+[a-zA-Z]*)\s*(?:\dGB\s*RAM)*\s*(\d+)GB"
+
+
+def getModel():
+    modelo = []
+    for a in parsed_page.findAll('a', {'class':"name-link"}):
+        filteredName = re.sub('Smartphone Samsung ', '', a.text)
+        modelo.append(re.match(regexSamsung, filteredName).group(1))
+    return modelo
+
+def getCapacity():
+    capacidade = []
+    for a in parsed_page.findAll('a', {'class':"name-link"}):
+        filteredName = re.sub('Smartphone Samsung ', '', a.text)
+        capacidade.append(re.match(regexSamsung, filteredName).group(2))
+    return capacidade
+
+def getList():
+    numberOfFinds = len(parsed_page.findAll('a', {'class':"name-link"}))
+    listCelulares = []
+    modelos = getModel()
+    capacidades = getCapacity()
+    for i in range(numberOfFinds):
+        listCelulares.append(dict(zip(['modelo','capacidade'],[modelos[i],int(capacidades[i])])))
+    return listCelulares
