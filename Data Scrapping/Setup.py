4bd9f4d3ab93b8942b484bcea049ec137aad70bb
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-29 20:37:44 -0300

    Price and availability funcions working

--- Data Scrapping/Setup.py
+++ Data Scrapping/Setup.py
@@ -10,27 +10,50 @@ parsed_page = BeautifulSoup(open(url),"html.parser")
 
 
 regexSamsung = "(Galaxy S\d(?:\sPlus)?)\s*SM-G(?:\d+[a-zA-Z]*)\s*(?:\dGB\s*RAM)*\s*(\d+)GB"
+allProducts = parsed_page.findAll('a', {'class':"name-link"})
+allPrices = parsed_page.findAll('a', {'class':"price-label"})
+allStoresAvaliable = parsed_page.findAll('span', {'class' : "storeCount-txt"})
 
+def precoToFloat(x):
+    return float(re.sub(',','.',re.sub('[a-zA-Z\$\s\.]+','',x)))
 
 def getModel():
     modelo = []
-    for a in parsed_page.findAll('a', {'class':"name-link"}):
+    for a in allProducts:
         filteredName = re.sub('Smartphone Samsung ', '', a.text)
         modelo.append(re.match(regexSamsung, filteredName).group(1))
     return modelo
 
 def getCapacity():
     capacidade = []
-    for a in parsed_page.findAll('a', {'class':"name-link"}):
+    for a in allProducts:
         filteredName = re.sub('Smartphone Samsung ', '', a.text)
         capacidade.append(re.match(regexSamsung, filteredName).group(2))
     return capacidade
+
+def getLowestPrice():
+    menorPreco = []
+    for a in allPrices:
+        filteredPrice = precoToFloat(a.text)
+        menorPreco.append(filteredPrice)
+    return menorPreco
+
+def getAvailability():
+    disponibilidade = []
+    for a in allStoresAvaliable:
+        filteredAvailability = re.sub('[a-zA-Z\$\s\.]+','',a.text)
+        disponibilidade.append(filteredAvailability)
+    return disponibilidade
 
 def getList():
     numberOfFinds = len(parsed_page.findAll('a', {'class':"name-link"}))
     listCelulares = []
     modelos = getModel()
     capacidades = getCapacity()
+    menorPrecos = getLowestPrice()
+    disponibilidade = getAvailability()
     for i in range(numberOfFinds):
-        listCelulares.append(dict(zip(['modelo','capacidade'],[modelos[i],int(capacidades[i])])))
+        listCelulares.append(dict(zip(['modelo','capacidade','menor preco','disponibilidade'],[modelos[i],int(capacidades[i]),menorPrecos[i], int(disponibilidade[i])])))
     return listCelulares
+
+print(getList())
