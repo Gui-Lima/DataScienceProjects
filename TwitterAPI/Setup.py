d49e6601a44267e7f2791256a61322ce2274eb18
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-30 19:56:48 -0300

    Sucesseful connected to twitter API

--- TwitterAPI/Setup.py
+++ TwitterAPI/Setup.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
-import tweepy as tp
+import tweepy as tweepy
 from functools import reduce
 from collections import Counter
 import json
 
+with open('/home/ggfl/Github/DataScienceProjects/TwitterAPI/configKeys.txt') as f:
+    chaves = json.load(f)
+
+# Autentica o usuario na API
+auth = tweepy.OAuthHandler(chaves['Consumer'][0], chaves['Consumer'][1])
+
+# Autentica o aplicativo
+auth.set_access_token(chaves['PW'][0], chaves['PW'][1])
+
+api = tweepy.API(auth, wait_on_rate_limit=False)
+
+print(api.trends_available())
