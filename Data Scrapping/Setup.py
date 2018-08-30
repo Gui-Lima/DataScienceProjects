60dcbe66f12cfd423d28a9dfa4c26df94768f921
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-30 19:45:35 -0300

    Refactoring

--- Data Scrapping/Setup.py
+++ Data Scrapping/Setup.py
@@ -11,7 +11,6 @@ urlJ7Page = "/home/ggfl/Github/DataScienceProjects/Data Scrapping/DataSets/galax
 urlS7Page = "/home/ggfl/Github/DataScienceProjects/Data Scrapping/DataSets/galaxyS7PageZoom"
 parsed_page = BeautifulSoup(open(urlS8Page),"html.parser")
 
-
 regexSamsung = "(Galaxy (?:S|J|A)\d(?:\s20\d\d)?(?:\sPlus|\sPro|\sPrime\d*|\sNeo|\sMetal|\sDuo|\sEdge(?:\sBlack Piano)?)?)\s*(?:(?:SM-)?(?:G|J|A)?(?:\d*[A-Z]*)?)?\s*(?:\dGB\s*RAM)?\s*(\d\d+)GB(?:\s\d+G)?"
 
 allProducts = parsed_page.findAll('a', {'class':"name-link"})
