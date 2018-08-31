f4b208e7e0bcf0e3d41ef48039786c77fa5969cc
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-31 15:28:09 -0300

    Tweet into dataframe function

--- TwitterAPI/Setup.py
+++ TwitterAPI/Setup.py
@@ -18,14 +18,41 @@ api = tweepy.API(auth, wait_on_rate_limit=False)
 
 
 
-def gettingCurrentTTBrasil():
+def getCurrentTTBrasil():
     trendtopics = api.trends_place(23424768)
     dfTrendTopics = pd.DataFrame(trendtopics[0]['trends'])
     return dfTrendTopics
 
-def gettingUserTweets(UserName):
+def getUserTweets(UserName):
     userTweets = api.user_timeline(screen_name=UserName)
     return userTweets
 
+def filtra_campos(tweet):    
+    dic = {a[0] : a[1] for a in tweet.items() if a[0] in ['id',
+                                    'created_at',
+                                    'favorite_count',
+                                    'in_reply_to_screen_name',
+                                    'in_reply_to_status_id',
+                                    'in_reply_to_user_id',
+                                    'lang',
+                                    'retweet_count',
+                                    'source',
+                                    'text',
+                                    'truncated']}
+    dic.update(tweet['entities'])
+    if tweet['place']:
+        dic.update({'place_'+a[0] : a[1] for a in tweet['place'].items() \
+                    if a[0] in ['country', 'name']})
+    dic.update({'user_'+a[0] : a[1] for a in tweet['user'].items() \
+                if a[0] in ['screen_name', 'id']})
+    return dic
+    
+def searchTweets(nameSearch, numberOfResults=100):
+    tweets = api.search(nameSearch,count = numberOfResults,result_type="recent")
+    return tweets
 
+
+def tweetIntoDataFrame(tweets):
+    dfTweets = pd.DataFrame(list(map(lambda x: filtra_campos(x._json), tweets)))
+    return dfTweets
 
